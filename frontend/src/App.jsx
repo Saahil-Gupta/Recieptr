321551bdfe07a290720f0fdc5f8d9cf264d5cc3a
Saahil Gupta <83634729+Saahil-Gupta@users.noreply.github.com>
2025-06-09 13:50:25 +0530

    React boilerplate for front end

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Home from './components/Home.jsx';
+
+export default function App() {
+    return (
+        <Home />
+    );
+}
---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+export default function Home() {
+    return (
+        <div className="bg-white text-gray-800 dark:bg-gray-700 dark:text-amber-50 p-4">
+            <div className="max-2xl text-black dark:text-white">
+                Hello
+            </div>
+        </div>
+    );
+}
